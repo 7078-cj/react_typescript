fcff2f4196db4b23f8abcb74ad3b379daba8b95a
7078-cj <7078ceejay@gmail.com>
2026-01-31 22:40:51 +0800

    template literals and exclude

---
+++ src/templateliterals/Toast.tsx
@@ -0,0 +1,15 @@
+type HorizontalPosition = 'left' | 'center' | 'right'
+type VerticalPosition = 'top' | 'center' | 'bottom'
+
+type ToastProps = {
+    position: Exclude<`${HorizontalPosition}  - ${VerticalPosition}`, 'center - center'> 
+    | 'center'
+}
+
+function Toast({position}: ToastProps) {
+  return (
+    <div>Notification position - {position}</div>
+  )
+}
+
+export default Toast
